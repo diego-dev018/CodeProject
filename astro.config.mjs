1e39c5cac4a0f839a9d0d1998a60dc23c4ae2e21
Diego Sanchez <diegosanchez.dev018@gmail.com>
2024-12-23 21:52:07 -0600

    Add markdown processing plugins to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
-  adapter: vercel()
+  adapter: vercel(),
+  markdown: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypeRaw],
+  },
 });
